183fa2906568c0d9eb9fb5db71c8ca32da02dd7a
jeswanth <jachu.k1212@gmail.com>
2024-10-29 07:24:55 +0530

    ListRace

--- listrace/src/App.js
+++ listrace/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './Components/Header';
 import TopArea from './Components/TopArea';
 import Hero from './Components/Hero';
+import ListTopics from './Components/ListTopics';
+import Works from './Components/Works';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Header />
       <TopArea />
       <Hero />
+      <ListTopics />
+      <Works />
     </div>
   );
 }
